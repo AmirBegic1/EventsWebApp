6de8dcf877f1d8cc1a7185f7ad761506b6ee762b
Amir Begic <amir.begic12@gmail.com>
2023-10-23 01:14:39 +0200

    fixed import

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,4 @@
-import { useRouter } from "next/react";
+import { useRouter } from "next/router";
 import { getEventById } from "../../dummy-data";
 import { Fragment } from "react";
 import EventSummary from "../../components/event-detail/event-summary";

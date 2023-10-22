c9e68132d652783244863571136459351bc6957f
Amir Begic <amir.begic12@gmail.com>
2023-10-22 02:43:42 +0200

    added styles

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,28 +1,32 @@
 import Link from "next/link";
+import classes from './event-item.module.css'
+
 function EventItem(props) {
   const { title, image, date, location, id } = props;
 
   const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
     day: 'numeric',
     month: 'long',
-    year: 'string',
+    year: 'numeric',
   });
 
   const formattedAddress = location.replace(', ', '\n');
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image}  alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <address>{formattedAddress}</address>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}> Explore Event</Link>
         </div>
       </div>

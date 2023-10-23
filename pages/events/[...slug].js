224f7091b54d41cb969be1fe089a8e762dcc9085
Amir Begic <amir.begic12@gmail.com>
2023-10-23 15:40:56 +0200

    implemented search logic

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,18 @@
+import { useRouter } from "next/router";
+
 function FilteredEventsPage() {
-    return(
-        <div>
-            <h1>Filtered Events</h1>
-        </div>
-    )
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+    if(!filterData){
+        return <p className="center" >Loading...</p>
+    }
+
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
 }
 
 export default FilteredEventsPage;

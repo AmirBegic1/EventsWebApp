3a46ae0cdb4b580b226db0a749d1d0fb93f72e62
Amir Begic <amir.begic12@gmail.com>
2023-10-23 16:04:20 +0200

    added error messages

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,17 +1,70 @@
 import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import { Fragment } from "react";
+import ResultsTitle from "../../components/events/result-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage() {
   const router = useRouter();
 
   const filterData = router.query.slug;
-    if(!filterData){
-        return <p className="center" >Loading...</p>
-    }
+  if (!filterData) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p className="center">Loading...</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events"> Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filterYear = filterData[0];
+  const filerMonth = filterData[1];
+
+  const numYear = +filterYear;
+  const numMonth = +filerMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your value!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="events"> Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className="center">No events found!</p>;
+  }
+
+  const date = new Date(numYear, numMonth - 1);
 
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
 

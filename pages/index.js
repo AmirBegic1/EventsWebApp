79feaf30206ec245f0c48bfec3a4e56af91f4ee7
Amir Begic <amir.begic12@gmail.com>
2023-10-27 01:52:00 +0200

    added firebase database

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
-function HomePage() {
-
-  const featuredEvenets = getFeaturedEvents();
+function HomePage(props) {
 
   return (
     <div>
       
-      <EventList items = {featuredEvenets}/>
+      <EventList items = {props.events}/>
     </div>
   );
+}
+
+export async function getStaticProps(){
+  const featuredEvenets = await getFeaturedEvents();
+
+  return {
+    props:{
+      events: featuredEvenets
+    }
+  }
 }
 
 export default HomePage;
